40b61b4fe5f0808d45f6702c9579cbb85ccc6a50
dakarakus <vladan.dakic@gmail.com>
2025-06-11 12:00:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,4 @@ def update_map(hoverData):
         return map_fig
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=False, host='0.0.0.0', port=port)
+    app.run_server(debug=True)
